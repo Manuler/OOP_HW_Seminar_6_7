22bbd6f7952eb82799fe5008999ea213be71c8ea
Anton Manuylov <killsonic4@gmail.com>
2023-03-20 19:46:17 +0300

    Initial commit

---
+++ src/main/java/model/Fields.java
@@ -0,0 +1,9 @@
+package model;
+
+
+public enum Fields {
+    NAME,
+    SIRNAME,
+    TELEPHONE;
+
+}
---
+++ src/main/java/utils/PhoneException.java
@@ -0,0 +1,8 @@
+package utils;
+
+public class PhoneException extends Exception {
+    public PhoneException(String msg) {
+        super(msg);
+    }
+    
+}
